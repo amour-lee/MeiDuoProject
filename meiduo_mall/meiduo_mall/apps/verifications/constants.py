e7fae277723e292113846d9d9de632af7c81d627
Lee <15537186765@163.com>
2018-09-12 12:52:08 +0800

    注册之短信验证码优化（校验60s内是否重复发送短信)

--- meiduo_mall/meiduo_mall/apps/verifications/constants.py
+++ meiduo_mall/meiduo_mall/apps/verifications/constants.py
@@ -1,2 +1,5 @@
 # 短信验证码有效期 单位：秒
 SMS_CODE_REDIS_EXPIRES = 300
+
+# 发送短信时间间隔
+SEND_SMS_CODE_INTERVAL = 60
--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -5,6 +5,7 @@ from django_redis import get_redis_connection
 from . import constants
 from meiduo_mall.libs.yuntongxun.sms import CCP
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -19,14 +20,26 @@ class SMSCodeView(APIView):
         """
         GET /sms_codes/(?P<mobile>1[3-9]\d{9})/
         """
+
+        # 创建连接到redis的对象
+        redis_conn = get_redis_connection('verify_codes')
+
+        # 先判断用户在60s内是否重复发送短信
+        send_flag = redis_conn.get('send_flag_%s' % mobile)
+        if send_flag:
+            # 如果send_flag有值，说明60s内重复发送短信
+            return Response({'message': '频繁发送短信'}, status=status.HTTP_400_BAD_REQUEST)
+
         # 生成短信验证码
         sms_code = '%06d' % random.randint(0, 999999)
         logger.info(sms_code)
 
         # 保存短信验证码码到redis
-        redis_conn = get_redis_connection('verify_codes')
-        # redis_conn.setex('key','过期时间','value')
         redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+
+        # 在redis中添加一个发送短信的标记（值）
+        redis_conn.setex('send_flag_%s' % mobile, constants.SEND_SMS_CODE_INTERVAL, 1)
+
         # 使用容联云通讯发送短信验证码
         CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES // 60], 1)
 
