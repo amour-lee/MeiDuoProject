8ff5a5c85bc060a0376b4eb65357049359452c05
Lee <15537186765@163.com>
2018-09-14 16:00:12 +0800

    QQ登录之处理QQ扫码登录回调准备（回调页面，后端接口）

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
+    # 获取QQ登录扫码页面
     url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
+    # 处理QQ扫码登录回调
+    url(r'^qq/user/$', views.QQAuthUserView.as_view()),
 ]
