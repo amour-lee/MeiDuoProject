1eefa4a0bebcf1126de2ce1556246522aba1613c
Lee <15537186765@163.com>
2018-09-14 16:27:36 +0800

    QQ登录之处理QQ扫码登录回调（获取code,access_token,openid）

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -3,8 +3,13 @@ from rest_framework.views import APIView
 from QQLoginTool.QQtool import OAuthQQ
 from django.conf import settings
 from rest_framework.response import Response
+from rest_framework import status
+import logging
 # Create your views here.
 
+
+# 日志记录器
+logger = logging.getLogger('django')
 
 # url(r'^qq/user/$', views.QQAuthUserView.as_view()),
 class QQAuthUserView(APIView):
@@ -12,9 +17,27 @@ class QQAuthUserView(APIView):
 
     def get(self, request):
         # 提取code请求参数
-        # 使用code向QQ服务器请求access_token
-        # 使用access_token向QQ服务器请求openid
+        code = request.query_params.get('code')
+        if not code:
+            return Response({'message': '缺少code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 创建oauthQQ对象
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID, client_secret=settings.QQ_CLIENT_SECRET,
+                        redirect_uri=settings.QQ_REDIRECT_URI, state=next)
+
+        try:
+            # 使用code向QQ服务器请求access_token
+            access_token = oauth.get_access_token(code)
+
+            # 使用access_token向QQ服务器请求openid
+            openid = oauth.get_open_id(access_token)
+
+        except Exception as e:
+            logger.info(e)
+            return Response({'message': 'QQ服务器内部错误'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
         # 使用openid查询该QQ用户是否在美多商城中绑定过用户
+
         # 如果openid已绑定美多商城用户，直接生成JWT token，并返回
         # 如果openid没绑定美多商城用户，创建用户并绑定到openid
         pass
